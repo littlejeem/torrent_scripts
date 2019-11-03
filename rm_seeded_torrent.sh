2ab6a56179d505b8ef7c2f1436a65dae446a4ff0
James Livingstone <james@jamess-mbp.home>
2019-11-03 16:00:41 +0000

    set out if->then statements more aesthetically

--- rm_seeded_torrent.sh
+++ rm_seeded_torrent.sh
@@ -16,7 +16,8 @@ for ID in "$TORRENTLIST"
 do
   RATIO=`transmission-remote -n "$TR_ADMIN":"$TR_PASSWORD" --torrent "$ID" --info  | grep "Ratio:" | cut -s -d ":" -f2`
   echo "$RATIO"
-    if [ $RATIO != "None" ]; then
+    if [ $RATIO != "None" ];
+    then
       INT_RATIO=`echo $RATIO '*100' | bc -l | awk -F '.' '{ print $1; exit; }'`
 		  echo "$INT_RATIO"
       STATE_STOPPED=`transmission-remote -n "$TR_ADMIN":"$TR_PASSWORD" --torrent $ID --info | grep "State: Stopped\|Finished\|Idle\|Seeding"`
@@ -29,16 +30,18 @@ do
       echo "$PERIOD"
 		  PERIOD=${PERIOD#?}
 		  echo $PERIOD
-        if [ "$INT_RATIO" -ge "$LIMIT_RATIO" ]; then
-        #logger -p $LOG_FILE -t $LOG_APP "removing torrent $ID, name=${NAME:1}, ratio=$RATIO"
-			  REASON="1:1 Seed Complete"
-			  echo "removing torrent $ID, name=${NAME:1}, reason=$REASON, ratio=$RATIO, seeded time=$SEEDING"
-        . "$SCRIPT_PATH"/rm_torrent.sh "$ID" "$NAME" "$REASON"
-          elif [[ "$PERIOD" -ge "$SEED_LIMIT_TIME" ]]; then
-			    REASON="time limit"
+        if [ "$INT_RATIO" -ge "$LIMIT_RATIO" ];
+        then
+          #logger -p $LOG_FILE -t $LOG_APP "removing torrent $ID, name=${NAME:1}, ratio=$RATIO"
+			    REASON="1:1 Seed Complete"
 			    echo "removing torrent $ID, name=${NAME:1}, reason=$REASON, ratio=$RATIO, seeded time=$SEEDING"
           . "$SCRIPT_PATH"/rm_torrent.sh "$ID" "$NAME" "$REASON"
-          fi
-    fi
+            elif [[ "$PERIOD" -ge "$SEED_LIMIT_TIME" ]];
+            then
+			        REASON="time limit"
+			        echo "removing torrent $ID, name=${NAME:1}, reason=$REASON, ratio=$RATIO, seeded time=$SEEDING"
+              . "$SCRIPT_PATH"/rm_torrent.sh "$ID" "$NAME" "$REASON"
+            fi
+        fi
 done
 exit 0
