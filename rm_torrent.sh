5ea47fa2f11f5b0c0c5970e5cc9a8ce1f9674708
James Livingstone <james@Jamess-MacBook-Pro.local>
2019-11-11 12:18:58 +0000

    pass repeated variables from rm_seeded_torrent.sh

--- rm_torrent.sh
+++ rm_torrent.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source /home/pi/bin/torrentscripts/my_password.sh
-transmission-remote -n $TR_ADMIN:"$TR_PASSWORD" --torrent $ID --remove-and-delete | cut -d "\"" -f2
-touch /home/pi/bin/torrentscripts/test.log
-echo "$(date) - DELETED - Reason:$3 - TorrentID:$1 - Name:$2" >> /home/pi/bin/torrentscripts/test.log
+transmission-remote -n "$TR_ADMIN":"$TR_PASSWORD" --torrent $ID --remove-and-delete | cut -d "\"" -f2
+touch $SCRIPT_PATH/rm_torrent.log
+echo "$(date) - DELETED - Reason:$3 - TorrentID:$1 - Name:$2" >> $SCRIPT_PATH/rm_torrent.log
