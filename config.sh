49f66d18bc78a6983646b03e497a13fa48ff3232
James Livingstone <james@jamess-mbp.home>
2019-11-11 10:47:10 +0000

    added additional variable for use

--- config.sh
+++ config.sh
@@ -3,4 +3,3 @@ TR_PASSWORD=""
 LIMIT_RATIO=""         # 100=1:1, 200=2:1 etc etc
 SEED_LIMIT_TIME=""     # times of seeding in seconds
 USERNAME="" #the users name in here, HINT: this is the name of the user running transmission-daemon
-BASEDIR="/home/$USERNAME/bin/torrent_scripts"
