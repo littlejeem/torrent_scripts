945af0c2f63a7f367d79c4e1740b8f853b485e76
James Livingstone <james@jamess-mbp.home>
2019-11-18 16:32:32 +0000

    errors in code so regressed to working version

--- rm_torrent.sh
+++ rm_torrent.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 transmission-remote -n "$TR_ADMIN":"$TR_PASSWORD" --torrent $ID --remove-and-delete | cut -d "\"" -f2
-touch "$HOME"/bin/torrent_scripts/rm_torrent.log
-echo "$(date) - DELETED - Reason:$3 - TorrentID:$1 - Name:$2" >> "$HOME"/bin/torrent_scripts/rm_torrent.log
+touch "$SCRIPT_PATH"/rm_torrent.log
+echo "$(date) - DELETED - Reason:$3 - TorrentID:$1 - Name:$2" >> "$SCRIPT_PATH"/rm_torrent.log
