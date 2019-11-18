2db15e002f24fd6c542ac9f9029f66bfb1dbc8e5
James Livingstone <james@jamess-mbp.home>
2019-11-18 16:31:08 +0000

    further testing needed so regressed to working version

--- rm_seeded_torrent.sh
+++ rm_seeded_torrent.sh
@@ -1,47 +1,35 @@
 #!/bin/bash
-#
-#
-##############################
-### set & import variables ###
-##############################
-VERISON="1.0"
-source "$HOME"/bin/torrent_scripts/config.sh
-TORRENTLIST=`transmission-remote -n "$TR_ADMIN":"$TR_PASSWORD" -l | sed -e '1d;$d;s/^ *//' | cut -s -d " " -f1`
-#
-#
-####################
-### begin script ###
-####################
-for ID in "$TORRENTLIST"
+VERISON=1.0
+SCRIPT_PATH="$HOME/bin/torrent_scripts"
+source "$SCRIPT_PATH"/config.sh
+TORRENTLIST=`transmission-remote -n $TR_ADMIN:"$TR_PASSWORD" -l | sed -e '1d;$d;s/^ *//' | cut -s -d " " -f1`
+LIMIT_RATIO="100"
+SEED_LIMIT_TIME="604800" #equals 7 days
+for ID in $TORRENTLIST
 do
-  RATIO=`transmission-remote -n "$TR_ADMIN":"$TR_PASSWORD" --torrent "$ID" --info  | grep "Ratio:" | cut -s -d ":" -f2`
-  echo "$RATIO"
-    if [ $RATIO != "None" ];
-    then
-      INT_RATIO=`echo $RATIO '*100' | bc -l | awk -F '.' '{ print $1; exit; }'`
-		  echo "$INT_RATIO"
-      STATE_STOPPED=`transmission-remote -n "$TR_ADMIN":"$TR_PASSWORD" --torrent $ID --info | grep "State: Stopped\|Finished\|Idle\|Seeding"`
-		  echo "$STATE_STOPPED"
-      NAME=`transmission-remote -n "$TR_ADMIN":"$TR_PASSWORD" --torrent $ID -i | grep "Name:" | cut -s -d ":" -f2`
-      echo "$NAME"
-		  SEEDING=`transmission-remote -n "$TR_ADMIN":"$TR_PASSWORD" --torrent $ID --info | grep "Seeding Time:" | cut -s -d ":" -f2`
-      echo "$SEEDING"
-		  PERIOD=`echo $SEEDING | cut -s -d " " -f3`
-      echo "$PERIOD"
-		  PERIOD=${PERIOD#?}
-		  echo $PERIOD
-        if [ "$INT_RATIO" -ge "$LIMIT_RATIO" ];
-        then
-          #logger -p $LOG_FILE -t $LOG_APP "removing torrent $ID, name=${NAME:1}, ratio=$RATIO"
-			    REASON="1:1 Seed Complete"
-			    echo "removing torrent $ID, name=${NAME:1}, reason=$REASON, ratio=$RATIO, seeded time=$SEEDING"
-          . "$HOME"/bin/torrent_scripts/rm_torrent.sh "$ID" "$NAME" "$REASON"
-            elif [[ "$PERIOD" -ge "$SEED_LIMIT_TIME" ]];
-            then
-			        REASON="time limit"
-			        echo "removing torrent $ID, name=${NAME:1}, reason=$REASON, ratio=$RATIO, seeded time=$SEEDING"
-              . "$HOME"/bin/torrent_scripts/rm_torrent.sh "$ID" "$NAME" "$REASON"
+        RATIO=`transmission-remote -n $TR_ADMIN:"$TR_PASSWORD" --torrent $ID --info  | grep "Ratio:" | cut -s -d ":" -f2`
+        echo "ratio = "$RATIO
+        if [ $RATIO != "None" ]; then
+                INT_RATIO=`echo $RATIO '*100' | bc -l | awk -F '.' '{ print $1; exit; }'`
+                echo "converted to interger = " $INT_RATIO
+                STATE_STOPPED=`transmission-remote -n $TR_ADMIN:"$TR_PASSWORD" --torrent $ID --info | grep "State: Stopped\|Finished\|Idle\|Seeding"`
+                echo "torrent status = "$STATE_STOPPED
+                NAME=`transmission-remote -n $TR_ADMIN:"$TR_PASSWORD" --torrent $ID -i | grep "Name:" | cut -s -d ":" -f2`
+                echo "torrent name = "$NAME
+                SEEDING=`transmission-remote -n $TR_ADMIN:"$TR_PASSWORD" --torrent $ID --info | grep "Seeding Time:" | cut -s -d ":" -f2`
+                echo "seed time = "$SEEDING
+                PERIOD=`echo $SEEDING | cut -s -d " " -f3`
+                PERIOD=${PERIOD#?}
+                echo "seed time converted to secs = "$PERIOD
+                if [ "$INT_RATIO" -ge "$LIMIT_RATIO" ]; then
+                        REASON="Seed Ratio Complete"
+                        echo "removing torrent $ID, name=${NAME:1}, reason=$REASON, ratio=$RATIO, seeded time=$SEEDING"
+                        . "$SCRIPT_PATH"/rm_torrent.sh "$ID" "$NAME" "$REASON"
+                elif [[ $PERIOD -ge $SEED_LIMIT_TIME ]]; then
+                        REASON="time limit"
+                        echo "removing torrent $ID, name=${NAME:1}, reason=$REASON, ratio=$RATIO, seeded time=$SEEDING"
+                        . "$SCRIPT_PATH"/rm_torrent.sh "$ID" "$NAME" "$REASON"
+                fi
         fi
-     fi
 done
 exit 0
